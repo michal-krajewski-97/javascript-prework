105b23549b50d7e80122cb30763e5fae75ab03db
michal.krajewski <michal.krajewski.7991@gmail.com>
2023-06-12 11:50:33 +0200

    who won

--- js/script.js
+++ js/script.js
@@ -29,3 +29,23 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if(computerMove == 'kamień' && playerMove == 'papier'){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == 'papier' && playerMove == 'nożyce'){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == 'nożyce' && playerMove == 'kamień'){
+    printMessage('Ty wygrywasz!');
+} else if(computerMove == 'papier' && playerMove == 'kamień'){
+    printMessage('Ja wygrywam!');
+} else if(computerMove == 'kamień' && playerMove == 'nożyce'){
+    printMessage('Ja wygrywam!');
+} else if(computerMove == 'nożyce' && playerMove == 'papier'){
+    printMessage('Ja wygrywam!');
+} else if(computerMove == 'kamień' && playerMove == 'kamień'){
+    printMessage('Mamy remis!');
+} else if(computerMove == 'papier' && playerMove == 'papier'){
+    printMessage('Mamy remis!');
+} else if(computerMove == 'nożyce' && playerMove == 'nożyce'){
+    printMessage('Mamy remis!');
+}
